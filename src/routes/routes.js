bfce99c8e8b1c9e1cfe59c93bccc3df0de39f1fa
scottgago <scottgago@gmail.com>
2017-05-28 15:01:02 -0700

    [CHORE]Things

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 var appointmentRoutes = require('./appointment-routes')
 var userRoutes = require('./user-routes')
 var authRoutes = require('./auth-routes')
-var notifyRoutes - require('./notify-routes')
+var notifyRoutes = require('./notify-routes')
 
 module.exports = {
 	registerRoutes: function(app, db, twilio_client, cache) {
