aa481fd7b9a6628ab6e958f87f54d0686afacca1
scottgago <scottgago@gmail.com>
2017-08-20 11:47:12 -0700

    sdf

--- client/appointments/components/AppointmentListAdmin.js
+++ client/appointments/components/AppointmentListAdmin.js
@@ -88,9 +88,7 @@ class AppointmentList extends Component {
 
 	return (
 		<div style={{width: '100%', overflowY: 'scroll', display: 'flex', flexDirection: 'column'}}>
-			<div stlyle={{width: '100%', top: 0, height: 64, zIndex: 0, backgroundColor: 'black'}} >
-
-			</div>
+			<div style={{width: '100%', top: 0, height: 64, zIndex: 0, backgroundColor: 'black'}} />
 			<List>
 				{appointments[0] && appointments[0].length && <Subheader style={{color: 'black'}} >{`Unassigned appointments ( ${appointments[0] && appointments[0].length || 0} )`}</Subheader> }
 			    { 
