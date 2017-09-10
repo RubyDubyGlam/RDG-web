efdad41afc31b37d8b43fd31ba1224c94845962f
scottgago <scottgago@gmail.com>
2017-09-10 13:20:22 -0700

    sdf

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -117,6 +117,8 @@ function registerRoutes(app, db) {
 						bcrypt.genSalt(10, function(err, salt) {
 							bcrypt.hash(password, salt, function(err, hash) {
 
+								console.log(user[0])
+
 								controller.set(
 									user[0]._id, 
 									{ password: hash }, 
